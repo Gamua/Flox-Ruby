c608458c46f74ed04603c3e5ee5962c498141604
Daniel Sperl <daniel@gamua.com>
2014-03-03 12:01:53 +0100

    enhanced documentation of the Flox class

--- lib/flox.rb
+++ lib/flox.rb
@@ -14,7 +14,7 @@ class Flox
   # @private
   attr_reader :service
 
-  # The player that is currently logged in.
+  # @return [Flox::Player] The player that is currently logged in.
   attr_reader :current_player
 
   # Creates a new instance with a certain game ID and key. Per default, a guest
@@ -26,12 +26,16 @@ class Flox
   end
 
   # Makes a key-login on the server. It is recommended to create a 'hero'
-  # player in the web interface and use that for the login.
+  # player in the web interface and use that for the login. After the login,
+  # `current_player` will point to this player.
+  # @return [Flox::Player]
   def login_with_key(key)
     login(:key, key)
   end
 
-  # Creates a new guest player and logs it in.
+  # Creates a new guest player and logs it in. After the login,
+  # `current_player` will point to this player.
+  # @return [Flox::Player]
   def login_guest()
     login(:guest)
   end
@@ -47,7 +51,7 @@ class Flox
 
   # Loads an entity with a certain type and id from the server.
   # Normally, the type is the class name you used for the entity in your game.
-  # Returns a Flox::Entity instance.
+  # @return [Flox::Entity]
   def load_entity(type, id)
     path = entity_path(type, id)
     data = service.get(path)
@@ -58,7 +62,14 @@ class Flox
     end
   end
 
+  # Loads an entity with type '.player'.
+  # @return [Flox::Player]
+  def load_player(id)
+    load_entity('.player', id)
+  end
+
   # Stores an entity on the server.
+  # @return [Flox::Entity]
   def save_entity(entity)
     result = service.put(entity.path, entity)
     entity['updatedAt'] = result['updatedAt']
@@ -66,11 +77,12 @@ class Flox
     entity
   end
 
-  # :call-seq:
-  #   delete_entity(entity)
-  #   delete_entity(type, id)
-  #
   # Deletes the given entity from the database.
+  # @overload delete_entity(entity)
+  #   @param [Flox:Entity] entity the entity to delete
+  # @overload delete_entity(type, id)
+  #   @param [String] type the type of the entity
+  #   @param [String] id the id of the entity
   def delete_entity(*entity)
     if entity.length > 1
       type, id = entity[0], entity[1]
@@ -91,6 +103,7 @@ class Flox
 
   # Loads all scores of a leaderboard, sorted by rank. 'scope' can either be
   # one of the symbols +:today, :this_week, :all_time+ or an array of player IDs.
+  # @return [Array<Flox::Score>]
   def load_scores(leaderboard_id, scope)
     path = leaderboard_path(leaderboard_id)
     args = {}
@@ -106,35 +119,37 @@ class Flox
   end
 
   # Loads a JSON object from the given path. This works with any resource
-  # e.g. entities, logs, etc. Always returns a Hash.
+  # e.g. entities, logs, etc.
+  # @return [Hash]
   def load_resource(path, args=nil)
     service.get(path, args)
   end
 
   # Loads a log with a certain ID. A log is a Hash instance.
+  # @return [Hash]
   def load_log(log_id)
     log = service.get log_path(log_id)
     log['id'] = log_id unless log['id']
     log
   end
 
-  # Loads logs, optionally restricted by a certain query.
+  # Loads logs defined by a certain query.
   # Here are some sample queries:
   #
-  # * 'day:2014-02-20' -> all logs of a certain day
-  # * 'severity:warning' -> all logs of type warning & error
-  # * 'severity:error' -> all logs of type error
-  # * 'day:2014-02-20 severity:error' all error logs from February 20th.
+  # * `day:2014-02-20` → all logs of a certain day
+  # * `severity:warning` → all logs of type warning & error
+  # * `severity:error` → all logs of type error
+  # * `day:2014-02-20 severity:error` → all error logs from February 20th.
   #
-  # Returns a Flox::ResourceEnumerator you can use to iterate
-  # over the logs.
+  # @return [Flox::ResourceEnumerator<Hash>]
   def load_logs(query=nil, limit=nil)
     log_ids = load_log_ids(query, limit)
     paths = log_ids.map { |log_id| log_path(log_id) }
     ResourceEnumerator.new(service, paths)
   end
 
-  # Loads just the IDs of the logs, restricted by a certain query.
+  # Loads just the IDs of the logs, defined by a certain query.
+  # @return [Array<String>]
   def load_log_ids(query=nil, limit=nil)
     log_ids = []
     cursor = nil
@@ -152,23 +167,26 @@ class Flox
     log_ids
   end
 
-  # Loads the status of the Flox service, which is a Hash with the
-  # keys 'status' and 'version'.
+  # Loads the status of the Flox service.
+  # @return [Hash] with the keys 'status' and 'version'.
   def status
     service.get("")
   end
 
   # The ID of the game you are accessing.
+  # @return [String]
   def game_id
     service.game_id
   end
 
   # The key of the game you are accessing.
+  # @return [String]
   def game_key
     service.game_key
   end
 
   # The base URL of the Flox service.
+  # @return [String]
   def base_url
     service.base_url
   end
--- lib/flox/version.rb
+++ lib/flox/version.rb
@@ -2,6 +2,10 @@
 ## Copyright: Copyright 2014 Gamua
 ## License:   Simplified BSD
 
+# The main class used to interact with the Flox cloud service. Create an
+# instance of Flox using the game ID and key acquired from the web interface,
+# then login with a "Hero" key. That way, you will be able to access the data
+# of all players.
 class Flox
 
   # The current version of the Flox SDK.
